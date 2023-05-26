647f49761e62b103c3f12d2c86edf66a31e0e19c
jmarcan286 <jmarcan286@iesalmudeyne.es>
2023-05-26 14:21:12 +0200

    Crear/Editar/Consultar persona jurísdica

--- tests/cypress/e2e/persona.cy.js
+++ tests/cypress/e2e/persona.cy.js
@@ -19,3 +19,83 @@ function navegarLogin() {
     cy.xpath("//button[@id='formLogin:acceder']/span").click();
     cy.wait(2000);
 }
+
+// Comando personalizado en Cypress para iterar
+Cypress.Commands.add('encontrarFila', (rutaTabla, buscarEnPrimeraColumna) => {
+    let filaPersona = 0;
+    cy.xpath(rutaTabla) // Selector de filas de la tabla
+    .each(($fila, indice) => {
+        cy.wrap($fila)
+            .find('tr') // Selector de celdas dentro de la fila
+            .each(($celda, indiceCelda) => {
+                const textoCelda = $celda.text();
+
+                if (textoCelda.includes(buscarEnPrimeraColumna)) {
+                    filaPersona = indiceCelda;
+                    return false; // Detener la iteración una vez encontrado el elemento
+                }
+            });
+    })
+    .then(() => {
+        
+    });
+});
+
+
+//Crear/Editar/Consultar persona de tipo Jurídica con los datos obligatorios correspondientes --- JUAN MARTIN (NO TERMINADO)
+
+describe('Crear/Editar/Consultar persona jurísdica', () => {
+
+    it('Editar/Consultar persona jurísdica', () => {
+
+        //Editar persona Jurísdica
+        navegarLogin();
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").click();
+        cy.wait(1000);
+
+        let filaPersona = 0;
+        cy.xpath("//tbody[@id='formBuscador:tablaPersona_data']") // Selector de filas de la tabla
+            .each(($fila) => {
+                cy.wrap($fila)
+                    .find('tr') // Selector de celdas dentro de la fila
+                    .each(($celda, indiceCelda) => {
+                        const textoCelda = $celda.text();
+
+                        if (textoCelda.includes('Martin S.L')) {
+                            filaPersona = indiceCelda;
+                            return false; // Detener la iteración una vez encontrado el elemento
+                        }
+                    });
+            })
+            .then(() => {
+                cy.xpath(`//button[@id='formBuscador:tablaPersona:${filaPersona}:edicionPersona']/span`).first().click();
+            });
+
+        /*const ruta = "//tbody[@id='formBuscador:tablaPersona_data']"
+        const busqueda = "Martin S.L"
+        const filaPersona = cy.encontrarFila(ruta, busqueda);
+        cy.xpath(`//button[@id='formBuscador:tablaPersona:${filaPersona}:edicionPersona']/span`).first().click();
+        */
+
+        cy.wait(1000);
+        cy.xpath("//input[@id='formFormulario:nif_cif_pj']").click().type("000000000");
+        cy.xpath("//button[@id='formFormulario:guardar']").click();
+        //Consultar persona Jurísdica
+        cy.xpath("//button[@id='formBuscador:tablaPersona:4:consultaPersona']/span").click();
+    })
+
+    it('Crear persona jurísdica', () => {
+
+        navegarLogin();
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").click();
+        cy.xpath("//button[@id='formBuscador:nuevapersona']/span[2]").click();
+        cy.xpath("//table[@id='formFormulario:valorTipoPersona']/tbody/tr/td[2]/div/div[2]/span").click();
+        cy.xpath("//input[@id='formFormulario:nombre_pj']").type("Martin S.L");
+        cy.xpath("//label[@id='formFormulario:comboValorIdentificadorPJ_label']").click();
+        cy.xpath("//li[@id='formFormulario:comboValorIdentificadorPJ_4']").click();
+        cy.xpath("//input[@id='formFormulario:nif_cif_pj']").type("MARTIN");
+        cy.xpath("//button[@id='formFormulario:guardar']").click();
+
+    })
+
+})
