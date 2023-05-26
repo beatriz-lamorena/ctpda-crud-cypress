86f0bff6f65a94f303e0b7725587189fea749d70
David <ddorluc760@iesalmudeyne.es>
2023-05-26 12:20:03 +0200

    Cambios Relacionar persona con expedientes

--- tests/cypress/e2e/persona.cy.js
+++ tests/cypress/e2e/persona.cy.js
@@ -19,3 +19,22 @@ function navegarLogin() {
     cy.xpath("//button[@id='formLogin:acceder']/span").click();
     cy.wait(2000);
 }
+
+//RELACIONAR PERSONA, DE CUALQUIER TIPO, CON EXPEDIENTES
+describe('Relacionar persona, de cualquier tipo, con expedientes', () => {
+    it('debería de relacionar las personas según sus expedientes', () =>{
+        navegarLogin();
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[4]/a/span").first().click();
+        cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").first().click();
+        cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").type('PS-2023/005');
+        cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").first().click();
+        cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").type('DAVID SL');
+        cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").first().click();
+        cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").type('DAVID');
+        cy.xpath("//button[@id='formListadoExpedientes:nuevoExpediente']/span[2]").first().click();
+        cy.wait(2000);
+        cy.xpath("//label[@id='formDialogExpedientes:comboTipoExpediente_label']").first().click();
+        cy.xpath("//li[@id='formDialogExpedientes:comboTipoExpediente_1']").first().click();
+        cy.xpath("//button[@id='formDialogExpedientes:validarTipExp']/span[2]").first().click();
+});
+});
