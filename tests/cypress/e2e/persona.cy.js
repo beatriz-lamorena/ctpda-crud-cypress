4af0cadb0cfb1d839f597a244d18b1dc601cf3bb
Antonio Leon <antleonfdez01@gmail.com>
2023-05-31 12:23:25 +0200

    mod realcionar repre a pers_juridica

--- tests/cypress/e2e/persona.cy.js
+++ tests/cypress/e2e/persona.cy.js
@@ -163,74 +163,74 @@ function navegarLogin() {
         cy.xpath("//button[@id='formFormulario:guardar']/span[2]").click();
       
         cy.wait(4000)
-      });
-      
-    })
-    
+    });
 
-    //Relacionar representante a personas de tipo jurídica -- Codigo: Antonio Leon ; Assertions: Juan Martín
-    describe('Relacionar representante a persona tipo jurídicas', () => {
-        it('Debería relacionar un representante a una persona tipo jurídica desde el formulario', () => {
-            navegarLogin();
-            //Entrar en el apartado de personas y validarlo - Juan Martin
-            cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").should('have.text','Personas').click();
-            //Editar un usuario creado haciendo click en el boton de 'Editar'
-            cy.xpath("//button[@id='formBuscador:tablaPersona:6:edicionPersona']/span").should('have.class','pi-pencil').first().click(); //Assertion: JM
-            //Verificar que esta marcada la opcion 'Juridica'
-            cy.xpath("//table[@id='formFormulario:valorTipoPersona']/tbody/tr/td[2]/div/div[2]").first().click().should('have.class', 'ui-state-active'); //Assertion: JM
-            //Buscar persona dentro del apartado representantes
-            cy.wait(2000);
-            cy.xpath("//button[@id='formFormulario:abrirBusquedaRepresentantes']/span[2]").should('be.visible').and('have.text', 'Buscar persona').first().click(); //Assertion: JM
-            //Añadir representante
-            cy.wait(2000);
-            cy.xpath("//button[@id='formFormulario:tablaNuevosRepresentantes:0:seleccionarRepre']/span").should('be.visible').and('have.class', 'pi-plus-circle').first().click(); //Assertion: JM
-            //Guardar la asignación de representante
-            cy.wait(2000);
-            cy.xpath("//button[@id='formFormulario:guardar']/span[2]").should('have.text', 'Guardar').first().click(); //Assertion: JM
-        });
+    })
+
+
+//Relacionar representante a personas de tipo jurídica -- Antonio Leon
+describe('Relacionar representante a persona tipo jurídicas', () => {
+    it('Debería relacionar un representante a una persona tipo jurídica desde el formulario', () => {
+        navegarLogin();
+        //Entrar en el apartado de personas y validarlo
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").should('have.text', 'Personas').click();
+        //Editar un usuario creado haciendo click en el boton de 'Editar'
+        cy.xpath("//button[@id='formBuscador:tablaPersona:6:edicionPersona']/span").should('have.class', 'pi-pencil').first().click(); //Assertion: JM
+        //Verificar que esta marcada la opcion 'Juridica'
+        cy.xpath("//table[@id='formFormulario:valorTipoPersona']/tbody/tr/td[2]/div/div[2]").first().click().should('have.class', 'ui-state-active'); //Assertion: JM
+        //Buscar persona dentro del apartado representantes
+        cy.wait(2000);
+        cy.xpath("//button[@id='formFormulario:abrirBusquedaRepresentantes']/span[2]").should('be.visible').and('have.text', 'Buscar persona').first().click(); //Assertion: JM
+        //Añadir representante
+        cy.wait(2000);
+        cy.xpath("//button[@id='formFormulario:tablaNuevosRepresentantes:0:seleccionarRepre']/span").should('be.visible').and('have.text', 'representante').first().click();
+        //Guardar la asignación de representante
+        cy.wait(2000);
+        cy.xpath("//button[@id='formFormulario:guardar']/span[2]").should('have.text', 'Guardar').first().click(); //Assertion: JM
     });
-    
-    /*//Borrar persona -- Pablo & Alvaro
-    describe('borrarPersona', () => {
-        it('borrar persona', () => {
-            navegarLogin();
-            //accedemos al apartado o menú de Personas
-            cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").click();
-            //Hacer click al botón de borrar persona (x)
-            cy.xpath("//button[@id='formBuscador:tablaPersona:2:deleteButton']/span").first().click();
-            //Confirmar borrado de la persona
-            cy.xpath("//button[@id='formBuscador:tablaPersona:4:j_idt85']/span").click();
-        });
+});
+
+//Borrar persona -- Pablo & Alvaro
+describe('borrarPersona', () => {
+    it('borrar persona', () => {
+        navegarLogin();
+        //accedemos al apartado o menú de Personas
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[13]/a").click();
+        //Hacer click al botón de borrar persona (x)
+        cy.xpath("//button[@id='formBuscador:tablaPersona:2:deleteButton']/span").first().click();
+        //Confirmar borrado de la persona
+        cy.xpath("//button[@id='formBuscador:tablaPersona:4:j_idt85']/span").click();
     });
-    
-    //RELACIONAR PERSONA, DE CUALQUIER TIPO, CON EXPEDIENTES -- David Dorante Lucas
-    describe('Relacionar persona, de cualquier tipo, con expedientes', () => {
-        it('debería de relacionar las personas según sus expedientes', () =>{
-            navegarLogin();
-            //Accedemos al apartado o menú de Generar Expedientes
-            cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[4]/a/span").first().click();
-            //Clickeamos en el apartado número expediente
-            cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").first().click();
-            //Rellenamos el campo número expediente
-            cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").type('PS-2023/005');
-            //Hacemos click en el campo persona
-            cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").first().click();
-            //Rellenamos el campo persona
-            cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").type('DAVID SL');
-            //Hacemos click en el campo NIF/CIF de la persona
-            cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").first().click();
-            //Rellenamos el campo NIF/CIF de la persona
-            cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").type('DAVID');
-            //Hacemos click en el botón Nuevo Expediente
-            cy.xpath("//button[@id='formListadoExpedientes:nuevoExpediente']/span[2]").first().click();
-            cy.wait(2000);
-            //Hacemos click sobre la barra Seleccione el tipo de expediente
-            cy.xpath("//label[@id='formDialogExpedientes:comboTipoExpediente_label']").first().click();
-            //Seleccionamos el tipo de expediente
-            cy.xpath("//li[@id='formDialogExpedientes:comboTipoExpediente_1']").first().click();
-            //Hacemos click en el botón guardar
-            cy.xpath("//button[@id='formDialogExpedientes:validarTipExp']/span[2]").first().click();
-        });
-    
-    }) */
+});
+
+//RELACIONAR PERSONA, DE CUALQUIER TIPO, CON EXPEDIENTES -- David Dorante Lucas
+describe('Relacionar persona, de cualquier tipo, con expedientes', () => {
+    it('debería de relacionar las personas según sus expedientes', () => {
+        navegarLogin();
+        //Accedemos al apartado o menú de Generar Expedientes
+        cy.xpath("//div[@id='menuForm:menuPuntoMenu']/ul/li[4]/a/span").first().click();
+        //Clickeamos en el apartado número expediente
+        cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").first().click();
+        //Rellenamos el campo número expediente
+        cy.xpath("//input[@id='formListadoExpedientes:numeroExpediente_filtro']").type('PS-2023/005');
+        //Hacemos click en el campo persona
+        cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").first().click();
+        //Rellenamos el campo persona
+        cy.xpath("//input[@id='formListadoExpedientes:persona_filtro']").type('DAVID SL');
+        //Hacemos click en el campo NIF/CIF de la persona
+        cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").first().click();
+        //Rellenamos el campo NIF/CIF de la persona
+        cy.xpath("//input[@id='formListadoExpedientes:ident_persona_filtro']").type('DAVID');
+        //Hacemos click en el botón Nuevo Expediente
+        cy.xpath("//button[@id='formListadoExpedientes:nuevoExpediente']/span[2]").first().click();
+        cy.wait(2000);
+        //Hacemos click sobre la barra Seleccione el tipo de expediente
+        cy.xpath("//label[@id='formDialogExpedientes:comboTipoExpediente_label']").first().click();
+        //Seleccionamos el tipo de expediente
+        cy.xpath("//li[@id='formDialogExpedientes:comboTipoExpediente_1']").first().click();
+        //Hacemos click en el botón guardar
+        cy.xpath("//button[@id='formDialogExpedientes:validarTipExp']/span[2]").first().click();
+    });
+
+});
 
